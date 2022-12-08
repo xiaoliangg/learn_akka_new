0da65a8d77681d8059914dfaf83fe251ac12a372
yl <1535523313@qq.com>
2022-12-08 11:31:05 +0800

    Part 2: Creating the First Actor

---
+++ J_DeviceTemperatureIOT_part2to5/src/main/java/com/yl/example/IotMain.java
@@ -0,0 +1,11 @@
+package com.yl.example;
+
+import akka.actor.typed.ActorSystem;
+
+public class IotMain {
+
+    public static void main(String[] args) {
+        // Create ActorSystem and top level supervisor
+        ActorSystem.create(IotSupervisor.create(), "iot-system");
+    }
+}
