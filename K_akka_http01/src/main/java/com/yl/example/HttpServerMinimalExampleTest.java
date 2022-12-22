67e7d0d47f85e99b9860b7eff92c5de38395d6fa
yl <1535523313@qq.com>
2022-12-22 09:48:01 +0800

    akka http

---
+++ K_akka_http01/src/main/java/com/yl/example/HttpServerMinimalExampleTest.java
@@ -0,0 +1,51 @@
+package com.yl.example;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
+import akka.actor.typed.javadsl.*;
+
+import java.util.Optional;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+
+import java.util.concurrent.CompletionStage;
+
+/**
+ * https://doc.akka.io/docs/akka-http/current/introduction.html#routing-dsl-for-http-servers
+ */
+public class HttpServerMinimalExampleTest extends AllDirectives {
+
+    public static void main(String[] args) throws Exception {
+        // boot up server using the route as defined below
+        ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "routes");
+//        akka.actor.ActorSystem classicActorSystem = Adapter.toClassic(system);
+
+        final Http http = Http.get(system);
+
+        //In order to access all directives we need an instance where the routes are define.
+        HttpServerMinimalExampleTest app = new HttpServerMinimalExampleTest();
+
+        final CompletionStage<ServerBinding> binding =
+                http.newServerAt("localhost", 8080)
+                        .bind(app.createRoute());
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read(); // let it run until user presses return
+
+        binding
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
+
+    private Route createRoute() {
+        return concat(
+                path("hello", () ->
+                        get(() ->
+                                complete("<h1>Say hello to akka-http</h1>"))));
+    }
+}
